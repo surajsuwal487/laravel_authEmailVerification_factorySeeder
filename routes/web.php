6c7fd0c6fed0052ffed9b09c76b2d24e52c3ada9
suraj <surajsuwal487@gmail.com>
2022-09-05 20:14:48 +0545

    query buidler added

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+use App\Models\User;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+//you can also include eloquent thigs here
 Route::get('/', function () {
-    return view('welcome');
+    $users = QueryBuilder::for(User::class)
+        ->allowedFilters([
+            'id',
+            'name',
+            'email',
+            AllowedFilter::scope('verified'),
+            AllowedFilter::exact('id')
+
+        ])
+        ->allowedSorts([
+            'name'
+        ])
+        ->allowedFields(['id', 'name'])
+        ->get();
+
+    return $users;
 });
 
 Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/verify',[MailController::class, 'verifyUser'])->name('verify.user');
+Route::get('/verify', [MailController::class, 'verifyUser'])->name('verify.user');
